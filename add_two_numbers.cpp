0831d48271ef3041de09795908cce7c115e0e09b
NiharikaGajula <16wh1a0515@bvrithyderabad.edu.in>
2017-07-12 18:42:36 +0530

    Add files via upload

---
+++ add_two_numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1, num2, sum;
+    cout << "Enter two numbers to add\n";
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << "sum is " << sum << "\n";
+    return 0;
+}
